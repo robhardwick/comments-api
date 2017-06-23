ec92e4e5dc6f235c51e24d6cee7131986e0abb9d
Rob Hardwick <robhardwick@gmail.com>
2017-06-23 17:20:57 +0100

    Add Watson API task tests

--- comments/api/tests.py
+++ comments/api/tests.py
@@ -1,7 +1,13 @@
+import json
+from unittest.mock import patch
+
 from django.test import TestCase
 from django.conf import settings
 
+import requests
 from rest_framework.test import APIClient
+
+from .tasks import fetch_tone
 
 class AppTestCase(TestCase):
     """ API tests """
@@ -126,3 +132,55 @@ class AppTestCase(TestCase):
         # Perform request that should fail
         response = self.client.put('/api/9999/', format='json')
         self.assertEqual(response.status_code, 404)
+
+    @patch('comments.api.tasks.logger')
+    def test_comment_tone_unknown(self, mock_logger):
+        """ Test comment tone creation for an unknown comment object """
+        fetch_tone(9999)
+        mock_logger.error.assert_called_with('Unknown comment object in fetch tone task: 9999')
+
+    @patch('requests.get', side_effect=requests.exceptions.HTTPError())
+    @patch('comments.api.tasks.logger')
+    def test_comment_tone_request_failure(self, mock_logger, mock_requests):
+        """ Test comment tone request failure """
+        fetch_tone(1)
+        mock_logger.error.assert_called_with('Error response received from Watson API: ')
+
+    @patch('requests.get', side_effect=requests.exceptions.RequestException())
+    @patch('comments.api.tasks.logger')
+    def test_comment_tone_request_connection_failure(self, mock_logger, mock_requests):
+        """ Test comment tone request connection failure """
+        fetch_tone(1)
+        mock_logger.error.assert_called_with('Error requesting from Watson API: ')
+
+    @patch('requests.get')
+    @patch('comments.api.tasks.logger')
+    def test_comment_tone(self, mock_logger, mock_requests):
+        """ Test comment tone creation """
+        mock_requests.return_value.json.return_value = {
+            "document_tone": {
+                "tone_categories": [{
+                    "tones": [
+                        {"score": 0.24748, "tone_id": "anger"},
+                        {"score": 0.322559, "tone_id": "disgust"},
+                        {"score": 0.108639, "tone_id": "fear"},
+                        {"score": 0.105358, "tone_id": "joy"},
+                        {"score": 0.083174, "tone_id": "sadness"}
+                    ],
+                    "category_id": "emotion_tone",
+                }]
+            }
+        }
+
+        # Check fetch tone task succeeds
+        fetch_tone(1)
+        mock_logger.error.assert_not_called()
+
+        # Perform request that should succeed
+        response = self.client.get('/api/1/', format='json')
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+
+        # Check the returned tone is correct
+        self.assertIn('tone', data)
+        self.assertEqual(data['tone'], 'disgust')
